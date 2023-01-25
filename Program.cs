9acaa80dbce65ea8d70fce1f9d2b00523822e643
kwe <kwevans@gmail.com>
2023-01-25 14:45:38 +0000

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using VaultSharp;
+using VaultSharp.V1.AuthMethods.Token;
+
+Console.WriteLine("Grabbing secrets from a local Vault");
+
+var authMethod = new TokenAuthMethodInfo("aVaultToken"); // don't do this, grab it from environment or something!
+var vaultClientSettings = new VaultClientSettings("http://localhost:8200",authMethod);
+var vaultClient = new VaultClient(vaultClientSettings);
+
+var secret = await vaultClient.V1.Secrets.KeyValue.V2.ReadSecretAsync(path:"mobilesdk", mountPoint:"secret");
+
+Console.WriteLine(secret.Data.Data["apikey"]);
+Console.WriteLine("Done");
+
+
